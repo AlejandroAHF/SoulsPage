1d782d3904ad9873e9ce53438fa2b98fc6644724
Edbuin <edwingranillo2020@gmail.com>
2025-09-16 21:46:10 -0600

    footer de SoulsPage

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<PieDePagina></PieDePagina>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PieDePagina from './components/pieDePagina.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PieDePagina
   }
 }
 </script>
